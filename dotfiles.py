bf3365200e8cff169f223c83f450e84e19d5c243
Alexander Rouse <rousepalexander@gmail.com>
2021-02-16 20:13:13 -0500

    add utility scripts

--- dotfiles.py
+++ dotfiles.py
@@ -15,26 +15,31 @@ files = [
   ".config/i3/config",
   ".config/i3status/config",
   ".config/dunst/dunstrc",
-  ".local/bin/i3statusxt"
+  ".local/bin/i3statusxt",
+  ".local/bin/updatedisplay",
+  ".local/bin/blurlock",
+  ".local/bin/mousespeed",
+  ".local/bin/resetiwl",
+  ".local/bin/volumeup"
 ]
 
 def warn(question):
   danger = input(question)
   return danger in [ "y", "Y", "yes", "Yes" ]
 
-def something(question, to, fromm):
+def safe_copy(question, to, fromm):
   if warn(question):
     for file in files:
       try:
         copyfile(to(file), fromm(file))
       except FileNotFoundError:
-        pass
+        print("can't find %s" % file)
 
 def copy(to_git):
   if to_git:
-    something("Copy from git to home? ", lambda x: x, lambda x: os.path.expanduser(f"~/{x}"))
+    safe_copy("Copy from git to home? ", lambda x: x, lambda x: os.path.expanduser(f"~/{x}"))
   else:
-    something("Copy from home to git? ", lambda x: os.path.expanduser(f"~/{x}"), lambda x: x)
+    safe_copy("Copy from home to git? ", lambda x: os.path.expanduser(f"~/{x}"), lambda x: x)
 
 def diff():
   for file in files:
@@ -42,7 +47,7 @@ def diff():
       if not filecmp.cmp(file, os.path.expanduser(f"~/{file}")):
         print(f"{file} changed")
     except FileNotFoundError:
-      pass
+      print("can't find %s" % file)
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
